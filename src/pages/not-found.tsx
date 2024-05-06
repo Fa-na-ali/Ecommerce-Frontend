426c8f8caaeeb4a68b505175823c69e5177786d3
fathima <nabeelaalifathima@gmail.com>
2024-05-06 21:00:22 +0530

    initial

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,14 @@
+
+import { MdError } from "react-icons/md";
+
+const NotFound = () => {
+  return (
+    <div className='container not-found'>
+      <MdError />
+      <h1>Page not found</h1>
+
+    </div>
+  )
+}
+
+export default NotFound
